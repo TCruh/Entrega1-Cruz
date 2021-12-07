0ae478cd7ae780b75c3e8009e5cecc9b590ea62f
Tomas S. Cruz <tomako5000@gmail.com>
2021-12-07 12:28:32 -0300

    Cambio Templates y views

--- Proyecto/AppCoder/views.py
+++ Proyecto/AppCoder/views.py
@@ -15,19 +15,7 @@ def Template(self):
     
     mis_datos = {'nombre': nombre, 'apellido': apellido, 'mi_dict': mi_dict, 'lista': [1,2,3,4,5,6]}
     
-    # # # otra forma de render - otra forma de manejar archivo
-    # miHtml = open("C:\Source Code\Clase17\Clase17\plantillas\prueba.html")
-
-    # plantilla = Template(miHtml.read()) #Se carga en memoria nuestro documento, template1   
-    # ##OJO importar template y contex, con: from django.template import Template, Context
-
-    # miHtml.close() #Cerramos el archivo
-
-    # miContexto = Context(mis_datos) #EN este caso no hay nada ya que no hay parametros, IGUAL hay que crearlo
-
-    # documento = plantilla.render(miContexto) #Aca renderizamos la plantilla en documento
-    
-    plantilla = loader.get_template('AppCoder/Inicio.html')
+    plantilla = loader.get_template('C:/Tomas/Entrega/Proyecto/AppCoder/templates/AppCoder/Inicio.html')
 
     documento = plantilla.render(mis_datos)
 
